ea63145be277b7fbf4257905635aca4e4af4ffec
roi lin <fabien705cccc@gmail.com>
2019-10-01 23:48:41 +0800

    first commit

---
+++ src/views/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <Hello></Hello>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Hello from '../components/Hello.vue'
+
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
